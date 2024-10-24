fd5175ae76a3d10bae982b6f3c374c34181f58c9
Jennifer Uraga Lopez <jenny@jennifers-mbp.mynetworksettings.com>
2024-10-24 01:25:04 -0400

    Display list and add top bar

--- app/src/main/java/com/example/superheroesapp/MainActivity.kt
+++ app/src/main/java/com/example/superheroesapp/MainActivity.kt
@@ -38,12 +38,38 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun SuperheroesApp() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar()
+        }
+    ) {
+        /* Important: It is not a good practice to access data source directly from the UI.
+        In later units you will learn how to use ViewModel in such scenarios that takes the
+        data source as a dependency and exposes heroes.
+         */
+        val heroes = HeroesRepository.heroes
+        HeroesList(heroes = heroes, contentPadding = it)
+    }
+}
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource(R.string.app_name),
+                style = MaterialTheme.typography.displayLarge,
+            )
+        },
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun SuperHeroesPreview() {
     SuperheroesTheme {
         SuperheroesApp()
     }
